385d9f3716660becba63a89e77e7d3c7b70e78b5
TestPanther <TestPanther@localhost>
2014-01-09 18:45:24 -0500

    moved stuff from f-list_http to f-list_json

--- f-list_json.c
+++ f-list_json.c
@@ -10,6 +10,87 @@ struct FListWebRequestData_ {
     guint timer;
 };
 
+//TODO: you're supposed to change spaces to "+" values??
+static void g_string_append_cgi(GString *str, GHashTable *table) {
+    GHashTableIter iter;
+    gpointer key, value;
+    gboolean first = TRUE;
+    g_hash_table_iter_init(&iter, table);
+    while(g_hash_table_iter_next(&iter, &key, &value)) {
+        purple_debug_info("flist", "cgi writing key, value: %s, %s\n", (gchar *)key, (gchar *)value);
+        if(!first) g_string_append(str, "&");
+        g_string_append_printf(str, "%s", purple_url_encode(key));
+        g_string_append(str, "=");
+        g_string_append_printf(str, "%s", purple_url_encode(value));
+        first = FALSE;
+    }
+}
+
+static void g_string_append_cookies(GString *str, GHashTable *table) {
+    GHashTableIter iter;
+    gpointer key, value;
+    gboolean first = TRUE;
+    g_hash_table_iter_init(&iter, table);
+    while(g_hash_table_iter_next(&iter, &key, &value)) {
+        if(!first) g_string_append(str, " ");
+        g_string_append_printf(str, "%s", purple_url_encode(key));
+        g_string_append(str, "=");
+        g_string_append_printf(str, "%s;", purple_url_encode(value));
+        first = FALSE;
+    }
+}
+
+//mostly shamelessly stolen from pidgin's "util.c"
+static gchar *http_request(const gchar *url, gboolean http11, gboolean post, const gchar *user_agent, GHashTable *req_table, GHashTable *cookie_table) {
+    GString *request_str = g_string_new(NULL);
+    gchar *address = NULL, *page = NULL, *user = NULL, *password = NULL;
+    int port;
+    
+    purple_url_parse(url, &address, &port, &page, &user, &password);
+    
+    g_string_append_printf(request_str, "%s /%s%s", (post ? "POST" : "GET"), page, (!post && req_table ? "?" : ""));
+    if(req_table && !post) g_string_append_cgi(request_str, req_table);
+    g_string_append_printf(request_str, " HTTP/%s\r\n", (http11 ? "1.1" : "1.0"));
+    g_string_append_printf(request_str, "Connection: close\r\n");
+    if(user_agent) g_string_append_printf(request_str, "User-Agent: %s\r\n", user_agent);
+    g_string_append_printf(request_str, "Accept: */*\r\n");
+    g_string_append_printf(request_str, "Host: %s\r\n", address);
+    
+    if(cookie_table) {
+        g_string_append(request_str, "Cookie: ");
+        g_string_append_cookies(request_str, cookie_table);
+        g_string_append(request_str, "\r\n");
+    }
+    
+    if(post) {
+        GString *post_str = g_string_new(NULL);
+        gchar *post = NULL;
+        
+        if(req_table) g_string_append_cgi(post_str, req_table);
+        
+        post = g_string_free(post_str, FALSE);
+        
+        purple_debug_info("flist", "posting (len: %d): %s\n", strlen(post), post);
+
+        g_string_append(request_str, "Content-Type: application/x-www-form-urlencoded\r\n");
+        g_string_append_printf(request_str, "Content-Length: %d\r\n", strlen(post));
+        g_string_append(request_str, "\r\n");
+        
+        g_string_append(request_str, post);
+        
+        g_free(post);
+    } else {
+        g_string_append(request_str, "\r\n");
+    }
+    
+    if(address) g_free(address);
+    if(page) g_free(page);
+    if(user) g_free(user);
+    if(password) g_free(password);
+    
+    return g_string_free(request_str, FALSE);
+}
+
 GHashTable* requests; //TODO: Replace this with a simple list, perhaps?
 
 void flist_web_request_cancel(FListWebRequestData *req_data) {
